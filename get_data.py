55e3ed27fffaa0b634d6c804f19ddfc8b08fd7d8
zakpaw <zakpaw36@gmail.com>
2020-11-13 14:53:58 +0100

    "added connection to mysql database and crated tabel covid19"

--- get_data.py
+++ get_data.py
@@ -1,9 +1,11 @@
 import pandas as pd
 from covid import Covid
 from decimal import Decimal
+import sqlalchemy as sqla
 import json
 
 
+# Extract
 class DecimalEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, Decimal):
@@ -11,7 +13,24 @@ class DecimalEncoder(json.JSONEncoder):
         return super(DecimalEncoder, self).default(obj)
 
 
-covid = Covid()
-data = json.dumps(covid.get_data(), cls=DecimalEncoder)
+data = json.dumps(Covid().get_data(), cls=DecimalEncoder)
+
 df = pd.read_json(data, convert_dates=['last_update'])
-print(df.head())
+print(df.head(), '\nlength:', len(df))
+
+# Load
+DB_LOC = 'mysql+pymysql://root:passwel@localhost/covid19docker'
+engine = sqla.create_engine(DB_LOC)
+
+table_name = 'covid19'
+
+df.to_sql(
+    table_name,
+    engine,
+    if_exists='replace',
+    index=False
+)
+
+result = engine.execute('show tables;')
+for _r in result:
+    print(_r)
